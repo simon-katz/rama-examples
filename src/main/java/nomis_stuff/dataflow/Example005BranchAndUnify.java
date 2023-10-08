1c08fb39173ac3a35e348c0ffdd8eb1e5427e2d9
Simon Katz <sk@nomistech.com>
2023-10-08 12:23:34 +0100

    Add dataflow examples

---
+++ src/main/java/nomis_stuff/dataflow/Example005BranchAndUnify.java
@@ -0,0 +1,37 @@
+// Example from https://redplanetlabs.com/docs/~/tutorial4.html
+// Run from Groovy REPL with:
+//   nomis_stuff.dataflow.ExampleNUnify.main(null)
+
+package nomis_stuff.dataflow;
+
+import com.rpl.rama.Block;
+import com.rpl.rama.ops.Ops;
+import com.rpl.rama.ops.OutputCollector;
+import com.rpl.rama.ops.RamaOperation0;
+
+public class Example005BranchAndUnify {
+    public static class Brancher implements RamaOperation0 {
+        @Override
+        public void invoke(OutputCollector collector) {
+            collector.emitStream("pizzaOrders", 1);
+            collector.emitStream("saladOrders", 10);
+            collector.emitStream("saladOrders", 11);
+        }
+    }
+
+    public static void unifyExample() {
+        Block.each(new Brancher()).outStream("pizzaOrders", "pizzaAnchor1", "*pizzaOrderSize")
+                .outStream("saladOrders", "saladAnchor1", "*saladOrderSize")
+                .each(Ops.DEC, "*saladOrderSize").out("*orderSize")
+                .anchor("saladAnchor2")
+                .hook("pizzaAnchor1")
+                .each(Ops.INC, "*pizzaOrderSize").out("*orderSize")
+                .anchor("pizzaAnchor2")
+                .unify("pizzaAnchor2", "saladAnchor2")
+                .each(Ops.PRINTLN, "*orderSize")
+                .execute();
+    }
+
+    public static void main(String[] args) throws Exception {
+        unifyExample();
+    }}
